7c685c583a1926d73554116d417883eed8fbff77
BH <benh.ldn2022@gmail.com>
2024-09-23 15:50:09 +0100

    Server things

--- src/server.py
+++ src/server.py
@@ -1,5 +1,7 @@
 from waitress import serve
 from src import create_app
 
+app=create_app()
+
 if __name__ == "__main__":
-    serve(create_app(), host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=5000)
